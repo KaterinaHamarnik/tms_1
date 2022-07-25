5b93b445785b8fc5113243e01153af9e89fe60a1
Kate <104315807+Molania@users.noreply.github.com>
2022-07-25 23:42:20 +0300

    Add files via upload

---
+++ MyFirstProject.java
@@ -0,0 +1,10 @@
+public class MyFirstProject {
+
+    public static void main(String[] args) {
+        int age = 19;
+        String name = "Катерина";
+        System.out.print(name + " " + age);
+
+    }
+
+}
